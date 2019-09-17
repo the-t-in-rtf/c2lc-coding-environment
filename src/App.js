4c6d1b5474d996cd7163b08cebacbc1fafc647e7
Simon Bates <simon@bitstructures.com>
2019-09-17 11:11:21 -0400

    C2LC-13: App with Interpreter and drawing area

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
+// @flow
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Interpreter from './Interpreter';
+import TurtleGraphics from './TurtleGraphics';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppState = {
+    program: Array<string>
+};
+
+export default class App extends React.Component<{}, AppState> {
+    interpreter: Interpreter;
+    turtleGraphicsRef: { current: null | TurtleGraphics };
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            program: [
+                "forward",
+                "left",
+                "forward",
+                "left",
+                "forward",
+                "left",
+                "forward",
+                "left"
+            ]
+        };
+
+        this.interpreter = new Interpreter(
+            {
+                forward: () => {
+                    if (this.turtleGraphicsRef.current !== null) {
+                        this.turtleGraphicsRef.current.forward(40);
+                    }
+                },
+                left: () => {
+                    if (this.turtleGraphicsRef.current !== null) {
+                        this.turtleGraphicsRef.current.turnLeft(90);
+                    }
+                },
+                right: () => {
+                    if (this.turtleGraphicsRef.current !== null) {
+                        this.turtleGraphicsRef.current.turnRight(90);
+                    }
+                }
+            }
+        );
+
+        this.turtleGraphicsRef = React.createRef<TurtleGraphics>();
+
+        this.handleClickRun = this.handleClickRun.bind(this);
+    }
+
+    handleClickRun: () => void;
+    handleClickRun() {
+        this.interpreter.run(this.state.program);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='c2lc-graphics'>
+                    <TurtleGraphics ref={this.turtleGraphicsRef} />
+                </div>
+                <button onClick={ this.handleClickRun }>Run</button>
+            </div>
+        );
+    }
 }
-
-export default App;
