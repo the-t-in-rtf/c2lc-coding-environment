56ad32acd09547fd63f0f5f024e08af957899d85
Daniel Cho <daniel@Daniels-MacBook-Pro.local>
2019-10-16 09:26:52 -0400

    C2LC-17: Revert FeatureDetection related to ClapDetector

--- src/FeatureDetection.js
+++ src/FeatureDetection.js
@@ -4,13 +4,4 @@ function bluetoothApiIsAvailable(): boolean {
     return !!((navigator: any).bluetooth);
 }
 
-function webAudioAPIIsAvailable(): boolean {
-    return !!((window: any).AudioContext || (window:any).webkitAudioContext);
-}
-
-function mediaDevicesIsAvailable(): boolean {
-    return !!((navigator: any).mediaDevices);
-}
-
-
-export { bluetoothApiIsAvailable, webAudioAPIIsAvailable, mediaDevicesIsAvailable };
+export { bluetoothApiIsAvailable };
