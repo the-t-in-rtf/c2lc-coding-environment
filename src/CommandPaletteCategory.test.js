17531c300a10976c8d9cb8f4b507e521dbce9867
Daniel Cho <daniel@daniels-mbp.ocad.ca>
2019-11-05 16:03:54 -0500

    C2LC-19: Update test case for CommandPaletteCategory to cover rendering different number of CommandPaletteCommand

--- src/CommandPaletteCategory.test.js
+++ src/CommandPaletteCategory.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import { Row } from 'react-bootstrap';
 import CommandPaletteCategory from './CommandPaletteCategory';
 import CommandPaletteCommand from './CommandPaletteCommand';
@@ -8,14 +8,37 @@ import CommandPaletteCommand from './CommandPaletteCommand';
 configure({ adapter: new Adapter()});
 
 test('number of commands rendered by CommandPaletteCategory should be equal to the number of CommandPaletteCommand', () => {
-    const wrapper = shallow(
+    const twoCommandsWrapper = shallow(
+        <CommandPaletteCategory>
+            <CommandPaletteCommand commandName='forward'/>
+            <CommandPaletteCommand commandName='left'/>
+        </CommandPaletteCategory>
+    );
+    let commands = twoCommandsWrapper.find(Row);
+    let commandsChildren = commands.getElement().props.children;
+    expect(commandsChildren).toHaveLength(2);
+
+    const threeCommandsWrapper = shallow(
         <CommandPaletteCategory>
             <CommandPaletteCommand commandName='forward'/>
             <CommandPaletteCommand commandName='left'/>
             <CommandPaletteCommand commandName='right'/>
         </CommandPaletteCategory>
     );
-    const commands = wrapper.find(Row);
-    const commandsChildren = commands.getElement().props.children;
+    
+    commands = threeCommandsWrapper.find(Row);
+    commandsChildren = commands.getElement().props.children;
     expect(commandsChildren).toHaveLength(3);
+
+    const fourCommandsWrapper = shallow(
+        <CommandPaletteCategory>
+            <CommandPaletteCommand commandName='forward' />
+            <CommandPaletteCommand commandName='left' />
+            <CommandPaletteCommand commandName='right' />
+            <CommandPaletteCommand commandName='none' />
+        </CommandPaletteCategory>
+    );
+    commands = fourCommandsWrapper.find(Row);
+    commandsChildren = commands.getElement().props.children;
+    expect(commandsChildren).toHaveLength(4);
 });
