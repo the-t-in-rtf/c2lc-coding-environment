8343c19b2479981a432d707f45471595ce17a1f5
Daniel Cho <daniel@daniels-mbp.ocad.ca>
2019-11-05 16:48:37 -0500

    C2LC-19: Update description for the test case

--- src/CommandPaletteCommand.test.js
+++ src/CommandPaletteCommand.test.js
@@ -7,7 +7,7 @@ import CommandPaletteCommand from './CommandPaletteCommand';
 
 configure({ adapter: new Adapter()});
 
-test('onChange property of CommandPaletteCommand component should change state of App component, as well as variant of itself', () => {
+test('onChange property of CommandPaletteCommand component should change its variant according to selectedCommandName', () => {
     const mockChangeHandler = jest.fn();
     const commandPaletteCommandWrapper = shallow(
         <CommandPaletteCommand 
@@ -18,12 +18,12 @@ test('onChange property of CommandPaletteCommand component should change state o
 
     const getVariantValue = () => (commandPaletteCommandWrapper.find(Button).getElement().props.variant);
 
-    // before a command is selected, initial state of selectedCommandName of App should be none and variant of the command button should be set to light
+    // before a command is selected, initial command button's variant should be set to light
     expect(getVariantValue()).toBe('light');
 
     const commandButton = commandPaletteCommandWrapper.find(Button);
 
-    // after a command is selected, state of selectedCommandName of App should be set to the command name and variant of the command button should be set to outline-primary
+    // after a command is selected, variant of the command button should be set to outline-primary
     commandButton.simulate('click');
     expect(mockChangeHandler.mock.calls.length).toBe(1);
     expect(mockChangeHandler.mock.calls[0][0]).toBe('forward');
@@ -31,7 +31,7 @@ test('onChange property of CommandPaletteCommand component should change state o
     commandPaletteCommandWrapper.update();
     expect(getVariantValue()).toBe('outline-primary');
     
-    // after a command is selected again, state of selectedCommandName of App should reset to none and variant of the command button to light
+    // after the same command is selected, variant of the command button should be reset to light
     commandButton.simulate('click');
     expect(mockChangeHandler.mock.calls.length).toBe(2);
     expect(mockChangeHandler.mock.calls[1][0]).toBe('none');
@@ -39,7 +39,7 @@ test('onChange property of CommandPaletteCommand component should change state o
     commandPaletteCommandWrapper.update();
     expect(getVariantValue()).toBe('light');
 
-    // when some other command is clicked 
+    // when another command is clicked, variant of the command button should be unchanged  
     commandPaletteCommandWrapper.setProps({selectedCommandName: 'left'});
     commandPaletteCommandWrapper.update();
     expect(getVariantValue()).toBe('light');
