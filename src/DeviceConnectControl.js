ed94d8288876e4a9d7f4817cb02130aeda936a2d
Daniel Cho <daniel@Daniels-MacBook-Pro.local>
2019-12-03 17:01:50 -0500

    C2LC-81: Add status icons

--- src/DeviceConnectControl.js
+++ src/DeviceConnectControl.js
@@ -1,22 +1,35 @@
 // @flow
 
 import * as React from 'react';
-import {FormattedMessage} from 'react-intl';
+import { injectIntl, FormattedMessage } from 'react-intl';
+import { Image, Spinner } from 'react-bootstrap';
 import type {DeviceConnectionStatus} from './types';
+import connected from './svg/Status_Connected.svg';
+import notConnected from './svg/Status_NotConnected.svg';
+import './DeviceConnectControl.css'
 
 type DeviceConnectControlProps = {
+    intl: any,
     children: React.Element<any>, // Button contents
     onClickConnect: () => void,
     connectionStatus: DeviceConnectionStatus
 };
 
-export default class DeviceConnectControl extends React.Component<DeviceConnectControlProps, {}> {
+class DeviceConnectControl extends React.Component<DeviceConnectControlProps, {}> {
     render() {
         return (
             <div>
                 <button onClick={this.props.onClickConnect}>{this.props.children}</button>
-                <FormattedMessage id={`DeviceConnectControl.${this.props.connectionStatus}`} />
+                {this.props.connectionStatus === 'connected' ? <Image src={connected} alt={this.props.intl.formatMessage({id:'DeviceConnectControl.connected'})} className='DeviceConnectControl__dash-status-svg' /> :
+                    this.props.connectionStatus === 'connecting' ?
+                        <>
+                            <Spinner className='DeviceConnectControl__dash-status-svg' animation='border' role='status' size='sm'/>
+                            <span className="sr-only"><FormattedMessage id={'DeviceConnectControl.connecting'} /></span>
+                        </> :
+                        <Image src={notConnected} alt={this.props.intl.formatMessage({id:'DeviceConnectControl.notConnected'})} className='DeviceConnectControl__dash-status-svg' />}
             </div>
         );
     }
 }
+
+export default injectIntl(DeviceConnectControl);
