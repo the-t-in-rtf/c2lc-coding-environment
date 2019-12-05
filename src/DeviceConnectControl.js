eaf3d46b9e34198cb9fe92216f28763c8966e545
Simon Bates <simon@bitstructures.com>
2019-12-05 16:58:54 -0500

    C2LC-91: Rename DeviceConnectControl disabled flag

--- src/DeviceConnectControl.js
+++ src/DeviceConnectControl.js
@@ -6,14 +6,14 @@ import { Button, Spinner } from 'react-bootstrap';
 import type {DeviceConnectionStatus} from './types';
 import { ReactComponent as StatusConnectedIcon } from './svg/Status_Connected.svg';
 import { ReactComponent as StatusNotConnectedIcon } from './svg/Status_NotConnected.svg';
-import './DeviceConnectControl.css'
+import './DeviceConnectControl.css';
 
 type DeviceConnectControlProps = {
     intl: any,
     children: React.Element<any>, // Button contents
-    bluetoothApiIsAvailable: boolean,
-    onClickConnect: () => void,
-    connectionStatus: DeviceConnectionStatus
+    disabled: boolean,
+    connectionStatus: DeviceConnectionStatus,
+    onClickConnect: () => void
 };
 
 class DeviceConnectControl extends React.Component<DeviceConnectControlProps, {}> {
@@ -57,7 +57,7 @@ class DeviceConnectControl extends React.Component<DeviceConnectControlProps, {}
             <div className='DeviceConnectControl'>
                 <Button
                     className='DeviceConnectControl__button'
-                    disabled={!this.props.bluetoothApiIsAvailable}
+                    disabled={this.props.disabled}
                     onClick={this.props.onClickConnect}>
                     {this.props.children}
                 </Button>
