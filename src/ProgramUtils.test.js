f39d74ecc5025b687762e9e8814a3017bed75732
Daniel Cho <daniel@Daniels-MacBook-Pro-2.local>
2020-02-20 09:21:52 -0500

    C2LC-107: Change test cases in ProgramUtils to use test.each, and add test cases for programIsEmpty

--- src/ProgramUtils.test.js
+++ src/ProgramUtils.test.js
@@ -9,92 +9,99 @@ function checkProgramEdit(inputBefore: Program, expected: Program,
     expect(result).toEqual(expected);
 };
 
-test('deleteStep', () => {
-    const testCases = [
-        { input: [], index: 0, expected: [] },
-        { input: [], index: 2, expected: [] },
-        { input: ['foo'], index: 0, expected: [] },
-        { input: ['foo'], index: 1, expected: ['foo'] },
-        { input: ['foo'], index: 2, expected: ['foo'] },
-        { input: ['foo', 'bar', 'baz'], index: 0, expected: ['bar', 'baz'] },
-        { input: ['foo', 'bar', 'baz'], index: 1, expected: ['foo', 'baz'] }
-    ];
-
-    for (const testCase of testCases) {
-        const input = testCase.input.slice();
-        const result = ProgramUtils.deleteStep(input, testCase.index);
-        checkProgramEdit(testCase.input, testCase.expected, input, result);
+test.each([
+    [[], 0, []],
+    [[], 2, []],
+    [['foo'], 0, []],
+    [['foo'], 1, ['foo']],
+    [['foo'], 2, ['foo']],
+    [['foo', 'bar', 'baz'], 0, ['bar', 'baz']],
+    [['foo', 'bar', 'baz'], 1, ['foo', 'baz']]
+])('deleteStep',
+    (input, index, expected) => {
+        expect.assertions(1);
+        const inputValues = input.slice();
+        const result = ProgramUtils.deleteStep(inputValues, index);
+        expect(result).toStrictEqual(expected);
     }
-});
+);
 
-test('expandProgram', () => {
-    const testCases = [
-        { input: [], length: 0, expected: [] },
-        { input: [], length: 1, expected: ['fill1'] },
-        { input: [], length: 2, expected: ['fill1', 'fill1'] },
-        { input: ['foo'], length: 0, expected: ['foo'] },
-        { input: ['foo'], length: 1, expected: ['foo'] },
-        { input: ['foo'], length: 2, expected: ['foo', 'fill1'] },
-        { input: ['foo'], length: 3, expected: ['foo', 'fill1', 'fill1'] }
-    ];
-
-    for (const testCase of testCases) {
-        const input = testCase.input.slice();
-        const result = ProgramUtils.expandProgram(input, testCase.length, 'fill1');
-        checkProgramEdit(testCase.input, testCase.expected, input, result);
+test.each([
+    [[], 0, []],
+    [[], 1, ['fill1']],
+    [[], 2, ['fill1', 'fill1']],
+    [['foo'], 0, ['foo']],
+    [['foo'], 1, ['foo']],
+    [['foo'], 2, ['foo', 'fill1']],
+    [['foo'], 3, ['foo', 'fill1', 'fill1']]
+])('expandProgram',
+    (input, length, expected) => {
+        expect.assertions(1);
+        const inputValues = input.slice();
+        const result = ProgramUtils.expandProgram(inputValues, length, 'fill1');
+        expect(result).toStrictEqual(expected);
     }
-});
+);
 
-test('insert', () => {
-    const testCases = [
-        { input: [], index: 0, expected: ['commmand1'] },
-        { input: [], index: 2, expected: ['fill1', 'fill1', 'commmand1'] },
-        { input: ['foo'], index: 0, expected: ['commmand1', 'foo'] },
-        { input: ['foo'], index: 1, expected: ['foo', 'commmand1'] },
-        { input: ['foo'], index: 2, expected: ['foo', 'fill1', 'commmand1'] },
-        { input: ['foo', 'bar'], index: 1, expected: ['foo', 'commmand1', 'bar'] }
-    ];
-
-    for (const testCase of testCases) {
-        const input = testCase.input.slice();
-        const result = ProgramUtils.insert(input, testCase.index,
-            'commmand1', 'fill1');
-        checkProgramEdit(testCase.input, testCase.expected, input, result);
+test.each([
+    [[], 0, ['command1']],
+    [[], 2, ['fill1', 'fill1', 'command1']],
+    [['foo'], 0, ['command1', 'foo']],
+    [['foo'], 1, ['foo', 'command1']],
+    [['foo'], 2, ['foo', 'fill1', 'command1']],
+    [['foo', 'bar'], 1, ['foo', 'command1', 'bar']]
+])('insert',
+    (input, index, expected) => {
+        expect.assertions(1);
+        const inputValues = input.slice();
+        const result = ProgramUtils.insert(inputValues, index, 'command1', 'fill1');
+        expect(result).toStrictEqual(expected);
     }
-});
+);
 
-test('overwrite', () => {
-    const testCases = [
-        { input: [], index: 0, expected: ['commmand1'] },
-        { input: [], index: 2, expected: ['fill1', 'fill1', 'commmand1'] },
-        { input: ['foo'], index: 0, expected: ['commmand1'] },
-        { input: ['foo'], index: 1, expected: ['foo', 'commmand1'] },
-        { input: ['foo'], index: 2, expected: ['foo', 'fill1', 'commmand1'] },
-        { input: ['foo', 'bar', 'baz'], index: 1, expected: ['foo', 'commmand1', 'baz'] }
-    ];
-
-    for (const testCase of testCases) {
-        const input = testCase.input.slice();
-        const result = ProgramUtils.overwrite(input, testCase.index,
-            'commmand1', 'fill1');
-        checkProgramEdit(testCase.input, testCase.expected, input, result);
+test.each([
+    [[], 0, ['command1']],
+    [[], 2, ['fill1', 'fill1', 'command1']],
+    [['foo'], 0, ['command1']],
+    [['foo'], 1, ['foo', 'command1']],
+    [['foo'], 2, ['foo', 'fill1', 'command1']],
+    [['foo', 'bar', 'baz'], 1, ['foo', 'command1', 'baz']]
+])('overwrite',
+    (input, index, expected) => {
+        expect.assertions(1);
+        const inputValues = input.slice();
+        const result = ProgramUtils.overwrite(inputValues, index, 'command1', 'fill1');
+        expect(result).toStrictEqual(expected);
     }
-});
+);
 
-test('trimEnd', () => {
-    const testCases = [
-        { input: [], expected: [] },
-        { input: ['foo'], expected: ['foo'] },
-        { input: ['trim1'], expected: [] },
-        { input: ['foo', 'trim1'], expected: ['foo'] },
-        { input: ['trim1', 'trim1'], expected: [] },
-        { input: ['trim1', 'foo'], expected: ['trim1', 'foo'] },
-        { input: ['trim1', 'foo', 'trim1'], expected: ['trim1', 'foo'] }
-    ];
-
-    for (const testCase of testCases) {
-        const input = testCase.input.slice();
-        const result = ProgramUtils.trimEnd(input, 'trim1');
-        checkProgramEdit(testCase.input, testCase.expected, input, result);
+test.each([
+    [[], []],
+    [['foo'], ['foo']],
+    [['trim1'], []],
+    [['foo', 'trim1'], ['foo']],
+    [['trim1', 'trim1'], []],
+    [['trim1', 'foo'], ['trim1', 'foo']],
+    [['trim1', 'foo', 'trim1'], ['trim1', 'foo']]
+])('trimEnd',
+    (input, expected) => {
+        expect.assertions(1);
+        const inputValues = input.slice();
+        const result = ProgramUtils.trimEnd(inputValues, 'trim1');
+        expect(result).toStrictEqual(expected);
     }
-});
+);
+
+test.each([
+    [[], true],
+    [['none', 'none'], true],
+    [['command1', 'none'], false],
+    [['none', 'none', 'command1'], false],
+    [['command1', 'none', 'command1'], false]
+])('programIsEmpty',
+    (input, expected) => {
+        expect.assertions(1);
+        const result = ProgramUtils.programIsEmpty(input);
+        expect(result).toBe(expected);
+    }
+);
