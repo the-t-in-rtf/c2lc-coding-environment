d84fb30f8a941f4f6d6d5051688feeedc78b3a23
Daniel Cho <daniel@Daniels-MacBook-Pro-2.local>
2020-02-20 17:00:28 -0500

    C2LC-113: Change AriaDisablingButton implementation to use React forwardRef

--- src/AriaDisablingButton.js
+++ src/AriaDisablingButton.js
@@ -1,47 +1,28 @@
-// @flow
-
 import * as React from 'react';
 import { Button } from 'react-bootstrap';
 import classNames from 'classnames';
 
-type AriaDisablingButtonProps = {
-    onClick: (evt: any) => void,
-    disabled: boolean,
-    className?: string,
-    disabledClassName: string,
-    children: React.Node
-};
-
-export default class AriaDisablingButton extends React.Component<AriaDisablingButtonProps, {}> {
-    handleClick = (evt: any) => {
-        if (!this.props.disabled) {
-            this.props.onClick(evt);
-        }
-    };
-
-    render() {
-        const {
-            onClick,
-            disabled,
-            className,
-            disabledClassName,
-            children,
-            ...otherProps
-        } = this.props;
-
+const AriaDisablingButton = React.forwardRef(
+    ({onClick, disabled, className, disabledClassName, children, ...props}, ref) => {
         const classes = classNames(
             className,
             disabled && disabledClassName
         );
 
-        return React.createElement(
-            Button,
-            Object.assign({
-                "onClick": this.handleClick,
-                "aria-disabled": disabled,
-                "className": classes
-            }, otherProps),
-            children
-        );
+        if (ref) {
+            props.ref = ref;
+        }
+
+        return (
+            <Button
+                onClick={disabled ? undefined : onClick}
+                aria-disabled={disabled}
+                className={classes}
+                {...props}>
+                {children}
+            </Button>
+        )
     }
-};
+)
+
+export default AriaDisablingButton;
