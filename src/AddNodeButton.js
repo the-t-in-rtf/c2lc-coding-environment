d21190dd6ab02ae48fabdd60bb7b5fd3761ddc5d
Daniel Cho <daniel@Daniels-MacBook-Pro-2.local>
2020-02-25 10:21:50 -0500

    C2LC-171: Add a node component that will render in between program steps

---
+++ src/AddNodeButton.js
@@ -0,0 +1,90 @@
+// @flow
+
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import {injectIntl} from 'react-intl';
+import { ReactComponent as AddIcon } from './svg/Add.svg';
+import './AddNodeButton.scss';
+
+type AddNodeButtonProps = {
+    commandSelected: ?boolean,
+    showButton: boolean,
+    programStepNumber: number,
+    intl: any,
+    onDrop: (e: SyntheticDragEvent<HTMLButtonElement>) => void,
+    onClick: (e: SyntheticEvent<HTMLButtonElement>) => void
+};
+
+type AddNodeButtonState = {
+    showNode: boolean
+}
+
+class AddNodeButton extends React.Component<AddNodeButtonProps, AddNodeButtonState> {
+    ref: any;
+
+    constructor(props: AddNodeButtonProps) {
+        super(props);
+        this.ref = React.createRef();
+        this.state = {
+            showNode: false
+        }
+    }
+
+    handleDragOver = (e: SyntheticDragEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        if (this.state.showNode === false) {
+            this.setState({
+                showNode: true
+            });
+        }
+    }
+
+    handleFocus = () => {
+        this.setState({
+            showNode: true
+        });
+    }
+
+    // $FlowFixMe
+    render() {
+        if (this.props.showButton || this.state.showNode) {
+            return(
+                <Button
+                    ref={this.ref}
+                    className='AddNodeButton__plus-button'
+                    id={`programBlock-${this.props.programStepNumber}`}
+                    data-stepnumber={this.props.programStepNumber}
+                    onClick={this.props.onClick}
+                    onDrop={this.props.onDrop}
+                    onDragOver={this.handleDragOver}>
+                    <AddIcon className='AddNodeButton__plus-button-svg' />
+                </Button>
+            )
+        } else if (!this.props.showButton) {
+            return(
+                <div
+                    className='AddNodeButton__plus-button--minimize'
+                    id={`programBlock-${this.props.programStepNumber}`}
+                    tabIndex={
+                        this.props.commandSelected ?
+                        '0' :
+                        '-1'}
+                    onFocus={this.handleFocus}
+                    onDragOver={
+                        this.props.commandSelected ?
+                            this.handleDragOver :
+                            undefined
+                    }
+                />
+            )
+        }
+    }
+
+    componentDidUpdate(prevProps: {}, prevState: AddNodeButtonState) {
+        if (this.state.showNode !== prevState.showNode) {
+            this.ref.current.focus();
+        }
+    }
+}
+
+export default injectIntl(AddNodeButton);
