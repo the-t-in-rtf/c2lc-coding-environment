4caadc6d4a86eeb32c0fedca36de4b6e6a2d2b07
Daniel Cho <daniel@Daniels-MacBook-Pro-2.local>
2020-03-03 16:52:42 -0500

    C2LC-173: Implement logic for positioning and hiding

--- src/ActionPanel.js
+++ src/ActionPanel.js
@@ -6,19 +6,49 @@ import {injectIntl} from 'react-intl';
 import './ActionPanel.scss';
 
 type ActionPanelProps = {
+    showActionPanel: boolean,
+    position: {
+        top: number,
+        left: number
+    },
     intl: any
 };
 
 class ActionPanel extends React.Component<ActionPanelProps, {}> {
+    actionPanelRef: { current: null | HTMLDivElement };
+    constructor(props) {
+        super(props);
+        this.actionPanelRef = React.createRef();
+    }
+
     render() {
+        const positionStyles = {
+            position: 'absolute',
+            top: this.props.position.top - 150,
+            left: this.props.position.left
+        }
         return (
-            <div className='ActionPanel__panel'>
+            <div className={
+                this.props.showActionPanel ?
+                'ActionPanel__panel' :
+                'ActionPanel__panel--hidden'}
+                style={positionStyles}
+                ref={this.actionPanelRef}>
                 <Button className='ActionPanel__action-buttons'/>
                 <Button className='ActionPanel__action-buttons'/>
                 <Button className='ActionPanel__action-buttons'/>
                 <Button className='ActionPanel__action-buttons'/>
             </div>
         )
+    }
+
+    componentDidUpdate() {
+        if (this.props.showActionPanel) {
+            let element = this.actionPanelRef.current;
+            if (element && element.scrollIntoView) {
+                element.scrollIntoView({ behavior: 'auto', block: 'nearest', inline: 'nearest' });
+            }
+        }
     }
 }
 
