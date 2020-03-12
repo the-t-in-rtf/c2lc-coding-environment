1b041a9164a97516f47ac511fd2a7f3250550397
Daniel Cho <daniel@Daniels-MacBook-Pro-2.local>
2020-03-12 10:36:42 -0400

    C2LC-173: Refactor common focus trap logic to Utils

--- src/Utils.js
+++ src/Utils.js
@@ -8,4 +8,34 @@ function generateId(prefix: string): string {
     return id;
 }
 
-export { generateId };
+function setFocusTrap(e: SyntheticKeyboardEvent<HTMLInputElement>, replaceIsActive: boolean) {
+    const tabKeyCode = 9;
+    const escKeyCode = 27;
+    if (replaceIsActive) {
+        const firstCommandButton = document.getElementById('command-block--forward');
+        const lastCommandButton = document.getElementById('command-block--left');
+        const replaceButton = document.getElementById('replaceAction');
+        if (firstCommandButton && lastCommandButton && replaceButton) {
+            if (e.keyCode === tabKeyCode) {
+                if (e.shiftKey && document.activeElement === replaceButton) {
+                    e.preventDefault();
+                    lastCommandButton.focus();
+                } else if (e.shiftKey && document.activeElement === firstCommandButton) {
+                    e.preventDefault();
+                    replaceButton.focus();
+                } else if (!e.shiftKey && document.activeElement === replaceButton) {
+                    e.preventDefault();
+                    firstCommandButton.focus();
+                } else if (!e.shiftKey && document.activeElement === lastCommandButton) {
+                    e.preventDefault();
+                    replaceButton.focus();
+                }
+            } else if (e.keyCode === escKeyCode) {
+                this.props.onSetReplaceIsActive(false);
+                replaceButton.focus();
+            }
+        }
+    }
+}
+
+export { generateId, setFocusTrap };
