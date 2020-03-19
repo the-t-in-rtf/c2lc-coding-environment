d848900ad099e16dd6221067218c64d713312fd1
Daniel Cho <daniel@Daniels-MacBook-Pro-2.local>
2020-03-19 15:45:44 -0400

    C2LC-173: Add test cases for FocusTrapManager

---
+++ src/FocusTrapManager.test.js
@@ -0,0 +1,54 @@
+// @flow
+
+import FocusTrapManager from './FocusTrapManager';
+
+function checkFocusTrapProps(
+    focusTrapManager: FocusTrapManager,
+    active: boolean,
+    trapCloseHandler: () => void,
+    elementSelectors: Array<string>,
+    returnElementSelector: string) {
+        expect(focusTrapManager).toHaveProperty('active', active);
+        expect(focusTrapManager).toHaveProperty('elementSelectors', elementSelectors);
+        expect(focusTrapManager).toHaveProperty('onFocusTrapClosed');
+        expect(focusTrapManager).toHaveProperty('returnElementSelector', returnElementSelector);
+}
+
+function mockSetFocusTrap(focusTrapManager, elementSelectors, returnElementSelector) {
+    focusTrapManager.setFocusTrap(()=>{}, elementSelectors, returnElementSelector);
+    return focusTrapManager;
+}
+
+describe('FocusManagerProperty', () => {
+    let focusTrapManager;
+    let mockKeyPressObject;
+    beforeEach(() => {
+        focusTrapManager = new FocusTrapManager();
+        mockKeyPressObject = {
+            keyDown: null,
+            shiftKey: false
+        };
+    });
+
+    test('Checking default values', () => {
+        expect.assertions(4);
+        checkFocusTrapProps(focusTrapManager, false, ()=>{}, [], null);
+    })
+
+    test.each([
+        [[], null],
+        [[], 'returnSelector'],
+        [['selector1'], null],
+        [['selector1'], 'returnSelector'],
+        [['selector1', 'selector2', 'selector3'], null],
+        [['selector1', 'selector2', 'selector3'], 'returnSelector']
+    ])('setFocusTrap and unsetFocusTrap',
+        (elementSelectors, returnSelector) => {
+            expect.assertions(8);
+            focusTrapManager = mockSetFocusTrap(focusTrapManager, elementSelectors, returnSelector);
+            checkFocusTrapProps(focusTrapManager, true, ()=>{}, elementSelectors, returnSelector);
+            focusTrapManager.unsetFocusTrap();
+            checkFocusTrapProps(focusTrapManager, false, ()=>{}, [], null);
+        }
+    );
+});
