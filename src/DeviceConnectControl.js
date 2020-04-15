0259b82f19d44307764311151f9025abc4554b89
Daniel Cho <daniel@Daniels-MacBook-Pro-2.local>
2020-04-15 16:12:45 -0400

    C2LC-179: Update Dash connection status Icon

--- src/DeviceConnectControl.js
+++ src/DeviceConnectControl.js
@@ -2,10 +2,10 @@
 
 import * as React from 'react';
 import { injectIntl } from 'react-intl';
-import { Button, Spinner } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import type {DeviceConnectionStatus} from './types';
-import { ReactComponent as StatusConnectedIcon } from './svg/Status_Connected.svg';
-import { ReactComponent as StatusNotConnectedIcon } from './svg/Status_NotConnected.svg';
+import { ReactComponent as DashConnectionIcon } from './svg/Dash-Small.svg';
+import { ReactComponent as ConnectingIcon } from './svg/Connecting.svg';
 import './DeviceConnectControl.scss';
 
 type DeviceConnectControlProps = {
@@ -18,32 +18,34 @@ type DeviceConnectControlProps = {
 
 class DeviceConnectControl extends React.Component<DeviceConnectControlProps, {}> {
     connectionStatusIcon() {
+        const dashIconStyle = {
+            opacity: '50%',
+            width: '2.5rem',
+            height: '2.5rem',
+            position: 'relative',
+            top: '0.1rem'
+        };
+
         switch (this.props.connectionStatus) {
             case 'connected':
+                dashIconStyle.opacity = '100%';
                 return (
                     <span
                         role='img'
                         aria-label={this.props.intl.formatMessage({id:'DeviceConnectControl.connected'})}>
-                        <StatusConnectedIcon className='DeviceConnectControl__status-icon'/>
+                        <DashConnectionIcon
+                            style={dashIconStyle}
+                        />
                     </span>
                 );
             case 'connecting':
                 return (
-                    <Spinner
-                        animation='border'
-                        role='status'
-                        className='DeviceConnectControl__status-icon'>
-                        <span className="sr-only">
-                            {this.props.intl.formatMessage({id:'DeviceConnectControl.connecting'})}
-                        </span>
-                    </Spinner>
-                );
-            case 'notConnected':
-                return (
-                    <span
-                        role='img'
-                        aria-label={this.props.intl.formatMessage({id:'DeviceConnectControl.notConnected'})}>
-                        <StatusNotConnectedIcon className='DeviceConnectControl__status-icon' />
+                    <span>
+                        <DashConnectionIcon
+                            style={dashIconStyle}
+                        />
+                        <ConnectingIcon
+                            className='DeviceConnectControl__status-icon'/>
                     </span>
                 );
             default:
@@ -59,15 +61,17 @@ class DeviceConnectControl extends React.Component<DeviceConnectControlProps, {}
         }
         return (
             <div className={classNames.join(' ')}>
-                <Button
-                    className='DeviceConnectControl__button'
-                    disabled={this.props.disabled}
-                    onClick={this.props.onClickConnect}>
-                    {this.props.children}
-                </Button>
                 <span className='DeviceConnectControl__status-icon-container'>
                     {this.connectionStatusIcon()}
                 </span>
+                <Button
+                    className='DeviceConnectControl__button'
+                    disabled={
+                        this.props.disabled ||
+                        this.props.connectionStatus === 'connected'}
+                    onClick={this.props.onClickConnect}>
+                    {this.props.children}
+                </Button>
             </div>
         );
     }
