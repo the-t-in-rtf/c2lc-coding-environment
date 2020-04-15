ffa0c1fe9b1ccc804985be33192687a4836689a0
Daniel Cho <daniel@Daniels-MacBook-Pro-2.local>
2020-04-15 16:10:39 -0400

    C2LC-179: Move the connect to Dash button to the App heading

--- src/App.js
+++ src/App.js
@@ -187,12 +187,16 @@ export default class App extends React.Component<{}, AppState> {
                     messages={messages[this.state.settings.language]}>
                 <div role='banner' className='App__heading-section'>
                     <Container>
-                        <Row>
-                            <Col>
-                                <h1 className='App__app-heading'>
-                                    <FormattedMessage id='App.appHeading'/>
-                                </h1>
-                            </Col>
+                        <Row className='App__app-heading'>
+                            <h1 className='App__app-heading-text'>
+                                <FormattedMessage id='App.appHeading'/>
+                            </h1>
+                            <DeviceConnectControl
+                                disabled={!this.appContext.bluetoothApiIsAvailable}
+                                connectionStatus={this.state.dashConnectionStatus}
+                                onClickConnect={this.handleClickConnectDash}>
+                                <FormattedMessage id='App.connectToDash' />
+                            </DeviceConnectControl>
                         </Row>
                     </Container>
                 </div>
@@ -202,14 +206,6 @@ export default class App extends React.Component<{}, AppState> {
                             {!this.appContext.bluetoothApiIsAvailable &&
                                 <BluetoothApiWarning/>
                             }
-                        </Col>
-                        <Col md='auto'>
-                            <DeviceConnectControl
-                                    disabled={!this.appContext.bluetoothApiIsAvailable}
-                                    connectionStatus={this.state.dashConnectionStatus}
-                                    onClickConnect={this.handleClickConnectDash}>
-                                <FormattedMessage id='App.connectToDash' />
-                            </DeviceConnectControl>
                         </Col>
                     </Row>
                     <Row className='App__program-section' noGutters={true}>
