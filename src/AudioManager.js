31f3a68134cdc2c40c4a79de42e00888387918c7
Daniel Cho <chosww@gmail.com>
2020-06-16 13:53:56 -0400

    C2LC-191: Work in progress

--- src/AudioManager.js
+++ src/AudioManager.js
@@ -1,35 +1,17 @@
 // @flow
 
-import forwardSound from './audio/Move.wav';
-import turnLeftSound from './audio/TurnLeft.wav';
-import turnRightSound from './audio/TurnRight.wav';
-import addMovementSound from './audio/AddMovement.wav';
-import deleteAllSound from './audio/DeleteAll.wav';
-import deleteMovementSound from './audio/DeleteMovement.wav';
-import moveToLeftSound from './audio/MoveToLeft.wav';
-import moveToRightSound from './audio/MoveToRight.wav';
-import playProgramSound from './audio/PlayProgram.wav';
-import replaceMovementSound from './audio/ReplaceMovement.wav';
-
 export default class AudioManager {
     audioLookUpTable: Object;
 
     constructor() {
         this.audioLookUpTable = {
-            forward: forwardSound,
-            left: turnLeftSound,
-            right: turnRightSound,
-            addMovement: addMovementSound,
-            deleteAll: deleteAllSound,
-            deleteCurrentStep: deleteMovementSound,
-            moveToPreviousStep: moveToLeftSound,
-            moveToNextStep: moveToRightSound,
-            run: playProgramSound,
-            replaceCurrentStep: replaceMovementSound
+            forward: new Audio('/audio/Move.wav'),
+            left: new Audio('/audio/TurnLeft.wav'),
+            right: new Audio('/audio/TurnRight.wav')
         };
     }
 
     playSound = (soundName: string) => {
-        new Audio(this.audioLookUpTable[soundName]).play();
+        this.audioLookUpTable[soundName].play();
     }
 };
