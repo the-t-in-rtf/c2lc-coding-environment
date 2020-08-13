51d47d0977bb1ceaa84a6123a7dd2ea75e9c25b1
Daniel Cho <chosww@gmail.com>
2020-08-13 12:55:26 -0400

    C2LC-195: Add 9by5 grid to the Scene

--- src/Scene.js
+++ src/Scene.js
@@ -4,7 +4,7 @@ import React from 'react';
 import * as C2lcMath from './C2lcMath';
 import RobotCharacter from './RobotCharacter';
 import { injectIntl } from 'react-intl';
-import { sceneMinX, sceneMinY, sceneWidth, sceneHeight } from './Scene.scss';
+import { sceneMinX, sceneMinY, sceneWidth, sceneHeight, numRow, numColumn, gridCellWidth } from './Scene.scss';
 import './Scene.scss';
 
 type SceneProps = {
@@ -73,6 +73,36 @@ class Scene extends React.Component<SceneProps, SceneState> {
         return Promise.resolve();
     }
 
+    drawGrid(numRow: number, numColumn: number, width: number) {
+        const grid = [];
+        // importing scss variable returns string type
+        let xOffset = parseInt(sceneMinX);
+        let yOffset = parseInt(sceneMinY);
+        for (let i=1;i<numRow;i++) {
+            yOffset = yOffset + parseInt(width);
+            grid.push(<line
+                className='Scene__grid-line'
+                key={`row-${i}`}
+                x1={`${xOffset}`}
+                y1={`${yOffset}`}
+                x2={`${width * numColumn}`}
+                y2={`${yOffset}`} />);
+        }
+        xOffset = parseInt(sceneMinX);
+        yOffset = parseInt(sceneMinY);
+        for (let i=1;i<numColumn;i++) {
+            xOffset = xOffset + parseInt(width);
+            grid.push(<line
+                className='Scene__grid-line'
+                key={`column-${i}`}
+                x1={`${xOffset}`}
+                y1={`${yOffset}`}
+                x2={`${xOffset}`}
+                y2={`${width * numRow}`} />);
+        }
+        return grid;
+    }
+
     render() {
         // Subtract 90 degrees from the character bearing as the character image is drawn upright when it is facing East
         const robotCharacterTransform = `translate(${this.state.location.x} ${this.state.location.y}) rotate(${this.state.directionDegrees - 90} 0 0)`;
@@ -86,6 +116,7 @@ class Scene extends React.Component<SceneProps, SceneState> {
                     <svg
                         xmlns='http://www.w3.org/2000/svg'
                         viewBox={`${sceneMinX} ${sceneMinY} ${sceneWidth} ${sceneHeight}`}>
+                        {this.drawGrid(numRow, numColumn, gridCellWidth)}
                         <RobotCharacter robotCharacterTransform={robotCharacterTransform}/>
                     </svg>
                 </span>
