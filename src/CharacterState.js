5616f982e482ccc58676cce27adc5735ee45e71e
Daniel Cho <chosww@gmail.com>
2020-08-21 16:53:02 -0400

    C2LC-199: Add path parameter to the CharacterState constructor, to draw the character's path on the Scene

--- src/CharacterState.js
+++ src/CharacterState.js
@@ -6,22 +6,30 @@ export default class CharacterState {
     xPos: number; // Positive x is East
     yPos: number; // Positive y is South
     directionDegrees: number; // 0 is North, 90 is East
+    path: Array<any>;
 
-    constructor(xPos: number, yPos: number, directionDegrees: number) {
+    constructor(xPos: number, yPos: number, directionDegrees: number, path: Array<any>) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.directionDegrees = directionDegrees;
+        this.path = path;
     }
 
     forward(distance: number): CharacterState {
         const directionRadians = C2lcMath.degrees2radians(this.directionDegrees);
         const xOffset = Math.sin(directionRadians) * distance;
         const yOffset = Math.cos(directionRadians) * distance;
-
+        const newPathSegment = {
+            x1: this.xPos,
+            y1: this.yPos,
+            x2: this.xPos + xOffset,
+            y2: this.yPos - yOffset
+        };
         return new CharacterState(
             this.xPos + xOffset,
             this.yPos - yOffset,
-            this.directionDegrees
+            this.directionDegrees,
+            this.path.concat([newPathSegment])
         );
     }
 
@@ -29,7 +37,8 @@ export default class CharacterState {
         return new CharacterState(
             this.xPos,
             this.yPos,
-            C2lcMath.wrap(0, 360, this.directionDegrees - amountDegrees)
+            C2lcMath.wrap(0, 360, this.directionDegrees - amountDegrees),
+            this.path
         );
     }
 
@@ -37,7 +46,8 @@ export default class CharacterState {
         return new CharacterState(
             this.xPos,
             this.yPos,
-            C2lcMath.wrap(0, 360, this.directionDegrees + amountDegrees)
+            C2lcMath.wrap(0, 360, this.directionDegrees + amountDegrees),
+            this.path
         );
     }
 }
--- src/CharacterState.test.js
+++ src/CharacterState.test.js
@@ -7,11 +7,16 @@ import * as C2lcMath from './C2lcMath';
 //       with Flow than casting the expect() result to 'any'.
 
 expect.extend({
-    toHaveCharacterState(received, xPos, yPos, directionDegrees) {
+    toHaveCharacterState(received, xPos, yPos, directionDegrees, path) {
+        const isEqualPath = received.path[0] != null && path[0] != null ?
+            C2lcMath.approxEqual(received.path[0].x2, path[0].x2, 0.0001)
+            && C2lcMath.approxEqual(received.path[0].y2, path[0].y2, 0.0001) :
+            received.path.length === path.length;
         const pass =
             C2lcMath.approxEqual(received.xPos, xPos, 0.0001)
             && C2lcMath.approxEqual(received.yPos, yPos, 0.0001)
-            && received.directionDegrees === directionDegrees;
+            && received.directionDegrees === directionDegrees
+            && isEqualPath;
         if (pass) {
             return {
                 message: () => {
@@ -19,6 +24,7 @@ expect.extend({
                         + `    xPos: ${xPos}\n`
                         + `    yPos: ${yPos}\n`
                         + `    directionDegrees: ${directionDegrees}\n`
+                        + `    path: ${path}\n`
                         + `Received: ${this.utils.printReceived(received)}`;
                 },
                 pass: true
@@ -30,6 +36,7 @@ expect.extend({
                         + `    xPos: ${xPos}\n`
                         + `    yPos: ${yPos}\n`
                         + `    directionDegrees: ${directionDegrees}\n`
+                        + `    path: ${path}\n`
                         + `Received: ${this.utils.printReceived(received)}`;
                 },
                 pass: false
@@ -39,25 +46,25 @@ expect.extend({
 });
 
 test('Given the character is facing East, then Forward movement should move East', () => {
-    (expect(new CharacterState(0, 0, 90).forward(100)): any).toHaveCharacterState(100, 0, 90);
+    (expect(new CharacterState(0, 0, 90, []).forward(100)): any).toHaveCharacterState(100, 0, 90, [{x1: 0, y1: 0, x2: 100, y2: 0}]);
 });
 
 test('Given the character is facing South, then Forward movement should move South', () => {
-    (expect(new CharacterState(0, 0, 180).forward(100)): any).toHaveCharacterState(0, 100, 180);
+    (expect(new CharacterState(0, 0, 180, []).forward(100)): any).toHaveCharacterState(0, 100, 180, [{x1: 0, y1: 0, x2: 0, y2: 100 }]);
 });
 
 test('Given the character is facing 30 degrees N of E, then Forward should move in that direction', () => {
-    (expect(new CharacterState(0, 0, 60).forward(100)): any).toHaveCharacterState(86.6025, -50, 60);
+    (expect(new CharacterState(0, 0, 60, []).forward(100)): any).toHaveCharacterState(86.6025, -50, 60, [{x1: 0, y1: 0, x2: 86.6025, y2: -50}]);
 });
 
 test('Turn Left moves anti-clockwise and wraps at 0', () => {
-    (expect(new CharacterState(0, 0, 90).turnLeft(60)): any).toHaveCharacterState(0, 0, 30);
-    (expect(new CharacterState(0, 0, 90).turnLeft(90)): any).toHaveCharacterState(0, 0, 0);
-    (expect(new CharacterState(0, 0, 90).turnLeft(120)): any).toHaveCharacterState(0, 0, 330);
+    (expect(new CharacterState(0, 0, 90, []).turnLeft(60)): any).toHaveCharacterState(0, 0, 30, []);
+    (expect(new CharacterState(0, 0, 90, []).turnLeft(90)): any).toHaveCharacterState(0, 0, 0, []);
+    (expect(new CharacterState(0, 0, 90, []).turnLeft(120)): any).toHaveCharacterState(0, 0, 330, []);
 });
 
 test('Turn Right moves clockwise and wraps at 360', () => {
-    (expect(new CharacterState(0, 0, 270).turnRight(60)): any).toHaveCharacterState(0, 0, 330);
-    (expect(new CharacterState(0, 0, 270).turnRight(90)): any).toHaveCharacterState(0, 0, 0);
-    (expect(new CharacterState(0, 0, 270).turnRight(120)): any).toHaveCharacterState(0, 0, 30);
+    (expect(new CharacterState(0, 0, 270, []).turnRight(60)): any).toHaveCharacterState(0, 0, 330, []);
+    (expect(new CharacterState(0, 0, 270, []).turnRight(90)): any).toHaveCharacterState(0, 0, 0, []);
+    (expect(new CharacterState(0, 0, 270, []).turnRight(120)): any).toHaveCharacterState(0, 0, 30, []);
 });
