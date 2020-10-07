95ab30471eb6e79b51916fec65c9fea4b1212cf2
Daniel Cho <chosww@gmail.com>
2020-10-07 17:05:02 -0400

    C2LC-198: Implement Refresh button

---
+++ src/RefreshButton.js
@@ -0,0 +1,35 @@
+// @flow
+
+import React from 'react';
+import AriaDisablingButton from './AriaDisablingButton';
+import { injectIntl } from 'react-intl';
+import type { IntlShape } from 'react-intl';
+import { ReactComponent as RefreshIcon } from './svg/Refresh.svg';
+import './RefreshButton.scss';
+
+type RefreshButtonProps = {
+    intl: IntlShape,
+    interpreterIsRunning: boolean,
+    disabled: boolean,
+    onClick: () => void
+};
+
+class RefreshButton extends React.Component<RefreshButtonProps, {}> {
+    render() {
+        return (
+            <AriaDisablingButton
+                aria-label={`${this.props.intl.formatMessage({id:'RefreshButton'})}`}
+                className={this.props.interpreterIsRunning ?
+                    'RefreshButton RefreshButton--pressed' :
+                    'RefreshButton'}
+                disabledClassName='RefreshButton--disabled'
+                disabled={this.props.disabled}
+                onClick={this.props.onClick}
+            >
+                <RefreshIcon className='RefreshButton-svg' />
+            </AriaDisablingButton>
+        );
+    }
+}
+
+export default injectIntl(RefreshButton);
