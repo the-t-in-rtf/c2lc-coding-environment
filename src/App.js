911f6e24e1e02b8a4805141e423e0a581d940b4e
Daniel Cho <chosww@gmail.com>
2020-10-26 16:09:46 -0400

    C2LC-241: Implement a function to render CommandBlocks

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ import Scene from './Scene';
 import AudioFeedbackToggleSwitch from './AudioFeedbackToggleSwitch';
 import PenDownToggleSwitch from './PenDownToggleSwitch';
 import { programIsEmpty } from './ProgramUtils';
-import * as Utils from './Utils';
 import type { DeviceConnectionStatus, Program, RobotDriver } from './types';
 import messages from './messages.json';
 import './App.scss';
@@ -60,7 +59,6 @@ export default class App extends React.Component<{}, AppState> {
     appContext: AppContext;
     dashDriver: RobotDriver;
     interpreter: Interpreter;
-    toCommandPaletteNoticeId: string;
     audioManager: AudioManager;
     focusTrapManager: FocusTrapManager;
 
@@ -98,7 +96,7 @@ export default class App extends React.Component<{}, AppState> {
             'forward',
             'moveCharacter',
             () => {
-                this.audioManager.playSound('forward');
+                this.audioManager.playSound('forward1');
                 this.setState((state) => {
                     return {
                         characterState: state.characterState.forward(
@@ -119,7 +117,7 @@ export default class App extends React.Component<{}, AppState> {
             'left',
             'moveCharacter',
             () => {
-                this.audioManager.playSound('left');
+                this.audioManager.playSound('left45');
                 this.setState((state) => {
                     return {
                         characterState: state.characterState.turnLeft(90)
@@ -137,7 +135,7 @@ export default class App extends React.Component<{}, AppState> {
             'right',
             'moveCharacter',
             () => {
-                this.audioManager.playSound('right');
+                this.audioManager.playSound('right45');
                 this.setState((state) => {
                     return {
                         characterState: state.characterState.turnRight(90)
@@ -154,8 +152,6 @@ export default class App extends React.Component<{}, AppState> {
         // For FakeRobotDriver, replace with:
         // this.dashDriver = new FakeRobotDriver();
         this.dashDriver = new DashDriver();
-
-        this.toCommandPaletteNoticeId = Utils.generateId('toCommandPaletteNotice');
 
         this.audioManager = new AudioManager(this.state.audioEnabled);
 
@@ -302,6 +298,30 @@ export default class App extends React.Component<{}, AppState> {
         });
     }
 
+    renderCommandBlocks = () => {
+        const commandNames = [
+            'forward1', 'forward2', 'forward3',
+            'left45', 'left90', 'left180',
+            'right45', 'right90', 'right180'];
+
+        const commandBlocks = [];
+
+        for (const [index, value] of commandNames.entries()) {
+            commandBlocks.push(
+                <CommandPaletteCommand
+                    key={`CommandBlock-${index}`}
+                    commandName={value}
+                    selectedCommandName={this.getSelectedCommandName()}
+                    audioManager={this.audioManager}
+                    onChange={this.handleCommandFromCommandPalette}
+                    onDragStart={this.handleDragStartCommand}
+                    onDragEnd={this.handleDragEndCommand}/>
+            )
+        }
+
+        return commandBlocks;
+    }
+
     render() {
         return (
             <IntlProvider
@@ -368,41 +388,17 @@ export default class App extends React.Component<{}, AppState> {
                             </div>
                         </div>
                         <Row className='App__program-section' noGutters={true}>
-                            <Col md={4} lg={3} className='pr-md-3 mb-3 mb-md-0'>
+                            <Col md={5} lg={4} className='pr-md-4 mb-4 mb-md-0'>
                                 <div className='App__command-palette'>
                                     <h2 className='App__command-palette-heading'>
                                         <FormattedMessage id='CommandPalette.movementsTitle' />
                                     </h2>
-                                    <div className='App__command-palette-command'>
-                                        <CommandPaletteCommand
-                                            commandName='forward'
-                                            selectedCommandName={this.getSelectedCommandName()}
-                                            audioManager={this.audioManager}
-                                            onChange={this.handleCommandFromCommandPalette}
-                                            onDragStart={this.handleDragStartCommand}
-                                            onDragEnd={this.handleDragEndCommand}/>
-                                    </div>
-                                    <div className='App__command-palette-command'>
-                                        <CommandPaletteCommand
-                                            commandName='right'
-                                            selectedCommandName={this.getSelectedCommandName()}
-                                            audioManager={this.audioManager}
-                                            onChange={this.handleCommandFromCommandPalette}
-                                            onDragStart={this.handleDragStartCommand}
-                                            onDragEnd={this.handleDragEndCommand}/>
-                                    </div>
-                                    <div className='App__command-palette-command'>
-                                        <CommandPaletteCommand
-                                            commandName='left'
-                                            selectedCommandName={this.getSelectedCommandName()}
-                                            audioManager={this.audioManager}
-                                            onChange={this.handleCommandFromCommandPalette}
-                                            onDragStart={this.handleDragStartCommand}
-                                            onDragEnd={this.handleDragEndCommand}/>
+                                    <div className='App__command-palette-commands'>
+                                        {this.renderCommandBlocks()}
                                     </div>
                                 </div>
                             </Col>
-                            <Col md={8} lg={9}>
+                            <Col md={7} lg={8}>
                                 <ProgramBlockEditor
                                     activeProgramStepNum={this.state.activeProgramStepNum}
                                     actionPanelStepIndex={this.state.actionPanelStepIndex}
