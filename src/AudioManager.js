8597de8cef22a0eceb91f1d5d60ee1fa22d00488
Tony Atkins <tony@raisingthefloor.org>
2020-10-29 16:42:54 +0100

    C2LC-234: Added additional octave recordings to avoid speeding up the arpeggiation too much.  Improved pitch algo to stay in a narrower range of octaves.

--- src/AudioManager.js
+++ src/AudioManager.js
@@ -41,10 +41,13 @@ export default class AudioManager {
 
         this.sampler = new Sampler({
             urls: {
+                "C0": "C0.wav",
                 "C1": "C1.wav",
                 "C2": "C2.wav",
                 "C3": "C3.wav",
-                "C4": "C4.wav"
+                "C4": "C4.wav",
+                "C5": "C5.wav",
+                "C6": "C6.wav"
             },
             // TODO: Make this configurable
             baseUrl: "/audio/long-bell/"
@@ -79,18 +82,31 @@ export default class AudioManager {
     }
 
     playSoundForCharacterState(characterState: CharacterState) {
+        // TODO: Extract position -> pitch algo and generate a diagram of the tuning.
         // Modified "Tonnetz" tuning, see https://en.wikipedia.org/wiki/Tonnetz for explanation and diagrams.
 
         // The centre note (xPos: 0, yPos: 0) is 440hz = A4 = 69.
 
-        // Every "row" is 4 tones from the next but stays within the same octave.
-        const xPitchOffset = (5 * characterState.xPos)  % 12;
+        // Every "column" is 7 tones from the next but stays within the same octave.  This results in a pattern that
+        // cycles through the full octave range every 12 squares.
+        const xPitchOffset = (7 * characterState.xPos) % 12;
 
-        // Every "column" is 7 tones from the next.
-        const yPitchOffset = (-7 * characterState.yPos);
 
-        const midiNote = 69 + xPitchOffset + yPitchOffset;
+        // Every "row" is 4 tones from the next but stays within the same octave.  This results in a pattern of three
+        // notes that repeats every three rows.
+        const yPitchOffset = (4 * characterState.yPos) % 12;
+
+        // To vary the range of notes without going too high or low, we use the repeating nature of the "row" pattern
+        // to divide the tuning into "octave bands" every three rows.  The middle band (yPos of -1, 0, or 1) is octave 3.
+        // The "band" above centre (yPos of 2, 3, or 4) is octave 4.  Anything higher is octave 5. The "band" below
+        // centre (yPos of -2, -3, or -4) is octave 2.  Anything lower is octave 1.
+        const boundedYpos = characterState.yPos > 0 ? Math.max(5, characterState.yPos) : Math.max(-9, characterState.yPos);
+        const octaveOffset = (Math.ceil((boundedYpos + 2) / 3)) * -1;
+        const octave = 5 + octaveOffset;
+
+        const midiNote = xPitchOffset + yPitchOffset + (12 * octave);
         const noteName: string = Midi(midiNote).toNote();
+
         this.playPitchedSample(noteName);
 
         // Pan left/right to suggest the relative horizontal position.
