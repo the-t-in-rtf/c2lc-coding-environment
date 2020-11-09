16c51d47826605766f31d098c1b8dd7aadbe62a5
Daniel Cho <chosww@gmail.com>
2020-11-09 17:24:55 -0500

    C2LC-246: Add a test set to check max value of ProgramSpeedController is equal to the length of values property array

--- src/ProgramSpeedController.test.js
+++ src/ProgramSpeedController.test.js
@@ -38,15 +38,26 @@ describe('When value from the controller changes', () => {
     test('Should call onChange callback with value from speedLookUp at index value-1', () => {
         expect.assertions(2);
         const mockOnChange = jest.fn();
-        const speedLookUp = [2000, 1500, 1000, 500, 250];
+        const values = [2000, 1500, 1000, 500, 250];
         const { wrapper } = createMountProgramSpeedController({
             onChange: mockOnChange,
-            speedLookUp
+            values
         });
         const programSpeedController = getProgramSpeedController(wrapper);
         const targetValue = 4;
         programSpeedController.simulate('change', { target: { value: targetValue}});
         expect(mockOnChange.mock.calls.length).toBe(1);
-        expect(mockOnChange.mock.calls[0][0]).toBe(speedLookUp[targetValue-1]);
+        expect(mockOnChange.mock.calls[0][0]).toBe(values[targetValue-1]);
     })
 });
+
+test('Maximum value of the controller should be equal to the length of values property', () => {
+    expect.assertions(1);
+    const values = [2000, 1500, 1000, 500, 250];
+    const { wrapper } = createMountProgramSpeedController({
+        onChange: () => {},
+        values
+    });
+    const programSpeedController = getProgramSpeedController(wrapper);
+    expect(programSpeedController.get(0).props.max).toBe(values.length);
+})
