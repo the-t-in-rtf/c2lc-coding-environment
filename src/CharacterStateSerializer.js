aa86fa8fe4bda6fe5feb059ad287a97a71599c0a
Daniel Cho <chosww@gmail.com>
2020-11-11 15:34:05 -0500

    C2LC-247: Cleanup CharacterStateSerializer and convert the lookup table property into a function

--- src/CharacterStateSerializer.js
+++ src/CharacterStateSerializer.js
@@ -3,109 +3,42 @@
 import CharacterState from './CharacterState';
 
 export default class CharacterStateSerializer {
-    characterToPositionLookUpTable: {[string]: number};
-    constructor() {
-        this.characterToPositionLookUpTable = {
-            Z: -26,
-            Y: -25,
-            X: -24,
-            W: -23,
-            V: -22,
-            U: -21,
-            T: -20,
-            S: -19,
-            R: -18,
-            Q: -17,
-            P: -16,
-            O: -15,
-            N: -14,
-            M: -13,
-            L: -12,
-            K: -11,
-            J: -10,
-            I: -9,
-            H: -8,
-            G: -7,
-            F: -6,
-            E: -5,
-            D: -4,
-            C: -3,
-            B: -2,
-            A: -1,
-            '0': 0,
-            a: 1,
-            b: 2,
-            c: 3,
-            d: 4,
-            e: 5,
-            f: 6,
-            g: 7,
-            h: 8,
-            i: 9,
-            j: 10,
-            k: 11,
-            l: 12,
-            m: 13,
-            n: 14,
-            o: 15,
-            p: 16,
-            q: 17,
-            r: 18,
-            s: 19,
-            t: 20,
-            u: 21,
-            v: 22,
-            w: 23,
-            x: 24,
-            y: 25,
-            z: 26
-        };
-    }
-
     serialize(characterState: CharacterState): string {
         let pathParam = '' +
-            this.getAlphabetCoord(characterState.xPos) +
-            this.getAlphabetCoord(characterState.yPos) +
-            this.getDirectionCoord(characterState.direction);
-        for (let i=0; i<characterState.path.length; i++) {
-            const currentPath = characterState.path[i];
-            const { x1, x2, y1, y2 } = currentPath;
+            this.encodePosition(characterState.xPos) +
+            this.encodePosition(characterState.yPos) +
+            this.encodeDirection(characterState.direction);
+        for ( const pathSegment of characterState.path ) {
+            const { x1, x2, y1, y2 } = pathSegment;
             pathParam +=
-                this.getAlphabetCoord(x1) +
-                this.getAlphabetCoord(y1) +
-                this.getAlphabetCoord(x2) +
-                this.getAlphabetCoord(y2);
+                this.encodePosition(x1) +
+                this.encodePosition(y1) +
+                this.encodePosition(x2) +
+                this.encodePosition(y2);
         }
         return pathParam;
     }
 
     deserialize(text: string): CharacterState {
-        const xPos = this.characterToPositionLookUpTable[text.charAt(0)];
-        const yPos = this.characterToPositionLookUpTable[text.charAt(1)];
-        const direction = this.getDirectionFromCoord(text.charAt(2));
-        if (xPos == null || yPos == null || direction == null ) {
-            throw Error(`Invalid character position xPos=${xPos}, yPos=${yPos}, direction=${direction}`);
-        }
+        const xPos = this.decodePosition(text.charAt(0));
+        const yPos = this.decodePosition(text.charAt(1));
+        const direction = this.decodeDirection(text.charAt(2));
         const path = [];
         // Split path segment part of the text every 4 characters
         const encodedPathSegments = text.substring(3).match(/.{4}/g);
         if (encodedPathSegments != null) {
-            for (let i=0; i<encodedPathSegments.length; i++) {
-                const x1 = this.characterToPositionLookUpTable[encodedPathSegments[i].charAt(0)];
-                const y1 = this.characterToPositionLookUpTable[encodedPathSegments[i].charAt(1)];
-                const x2 = this.characterToPositionLookUpTable[encodedPathSegments[i].charAt(2)];
-                const y2 = this.characterToPositionLookUpTable[encodedPathSegments[i].charAt(3)];
-                if (x1 != null && y1 != null && x2 != null && y2 != null) {
-                    path.push({ x1, y1, x2, y2 });
-                } else {
-                    throw Error(`Invalid path coordinates x1=${x1} y1=${y1} x2=${x2} y2=${y2}`);
-                }
+            for ( const pathSegment of encodedPathSegments ) {
+                const x1 = this.decodePosition(pathSegment.charAt(0));
+                const y1 = this.decodePosition(pathSegment.charAt(1));
+                const x2 = this.decodePosition(pathSegment.charAt(2));
+                const y2 = this.decodePosition(pathSegment.charAt(3));
+                path.push({ x1, y1, x2, y2 });
             }
         }
         return new CharacterState(xPos, yPos, direction, path);
     }
 
-    getDirectionCoord(direction: number) {
+    encodeDirection(direction: number): string {
         switch(direction) {
             case(0): return '0';
             case(1): return 'a';
@@ -115,12 +48,12 @@ export default class CharacterStateSerializer {
             case(5): return 'e';
             case(6): return 'f';
             case(7): return 'g';
-            default: return '';
+            default: throw new Error(`Unrecognized direction ${direction}`);
         }
     }
 
-    getDirectionFromCoord(directionCoord: string) {
-        switch(directionCoord) {
+    decodeDirection(character: string): number {
+        switch(character) {
             case('0'): return 0;
             case('a'): return 1;
             case('b'): return 2;
@@ -129,23 +62,35 @@ export default class CharacterStateSerializer {
             case('e'): return 5;
             case('f'): return 6;
             case('g'): return 7;
-            default: throw new Error(`Unrecognized direction coordinate ${directionCoord}`);
+            default: throw new Error(`Unrecognized direction character ${character}`);
         }
     }
 
-    getAlphabetCoord(positionComponent: number) {
+    encodePosition(positionComponent: number): string {
         if (positionComponent === 0) {
             return '0';
         } else if (positionComponent > 0 && positionComponent < 27) {
-            return String.fromCharCode(96+positionComponent);
+            return String.fromCharCode('a'.charCodeAt(0) - 1 + positionComponent);
         } else if (positionComponent > 26) {
             return 'z';
         } else if (positionComponent < 0 && positionComponent > -27) {
-            return String.fromCharCode(64+Math.abs(positionComponent));
+            return String.fromCharCode('A'.charCodeAt(0) - 1 + Math.abs(positionComponent));
         } else if (positionComponent < -26) {
             return 'Z';
-        } else  {
-            return '';
         }
+        throw new Error(`Position out of the range: ${positionComponent}`);
+    }
+
+    decodePosition(character: string): number {
+        if (character === '0') {
+            return 0;
+        }
+        const charCode = character.charCodeAt(0);
+        if (charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0)) {
+            return charCode - 'a'.charCodeAt(0) + 1;
+        } else if (charCode >= 'A'.charCodeAt(0) && charCode <= 'Z'.charCodeAt(0)) {
+            return (charCode - 'A'.charCodeAt(0) + 1) * -1;
+        }
+        throw new Error(`Unrecognized position character: '${character}'`);
     }
 };
--- src/CharacterStateSerializer.test.js
+++ src/CharacterStateSerializer.test.js
@@ -5,75 +5,94 @@ import CharacterState from './CharacterState';
 
 test('Serialize character state', () => {
     expect.assertions(5);
-    const characterStateSerializer = new CharacterStateSerializer();
-    let characterState = new CharacterState(0, 0, 2, []);
-    expect(characterStateSerializer.serialize(characterState)).toBe('00b');
-    characterState = new CharacterState(1, 1, 4, [{x1: 0, y1: 0, x2: 1, y2: 1}]);
-    expect(characterStateSerializer.serialize(characterState)).toBe('aad00aa');
-    characterState = new CharacterState(-1, -1, 7, [{x1: 0, y1: 0, x2: -1, y2: -1}]);
-    expect(characterStateSerializer.serialize(characterState)).toBe('AAg00AA');
-    characterState = new CharacterState(-29, -29, 0, []);
-    expect(characterStateSerializer.serialize(characterState)).toBe('ZZ0');
-    characterState = new CharacterState(29, 1000, 3, []);
-    expect(characterStateSerializer.serialize(characterState)).toBe('zzc');
+    const serializer = new CharacterStateSerializer();
+    expect(serializer.serialize(
+        new CharacterState(0, 0, 2, [])
+    )).toBe('00b');
+    expect(serializer.serialize(
+        new CharacterState(1, 1, 4, [{x1: 0, y1: 0, x2: 1, y2: 1}])
+    )).toBe('aad00aa');
+    expect(serializer.serialize(
+        new CharacterState(-1, -1, 7, [{x1: 0, y1: 0, x2: -1, y2: -1}])
+    )).toBe('AAg00AA');
+    expect(serializer.serialize(
+        new CharacterState(-29, -29, 0, [])
+    )).toBe('ZZ0');
+    expect(serializer.serialize(
+        new CharacterState(29, 1000, 3, [])
+    )).toBe('zzc');
 });
 
 test('Deserialize character state', () => {
     expect.assertions(5);
-    const characterStateSerializer = new CharacterStateSerializer();
-    let text = '00b';
-    expect(characterStateSerializer.deserialize(text)).toStrictEqual(new CharacterState(0,0,2,[]));
-    text = 'aab00aa';
-    expect(characterStateSerializer.deserialize(text)).toStrictEqual(new CharacterState(1,1,2,[{x1:0,y1:0,x2:1,y2:1}]));
-    text = 'AAc00AA';
-    expect(characterStateSerializer.deserialize(text)).toStrictEqual(new CharacterState(-1,-1,3,[{x1:0,y1:0,x2:-1,y2:-1}]));
-    text = '3ac00AA';
+    const serializer = new CharacterStateSerializer();
+    expect(serializer.deserialize('00b')).toStrictEqual(new CharacterState(0,0,2,[]));
+    expect(serializer.deserialize('aab00aa')).toStrictEqual(new CharacterState(1,1,2,[{x1:0,y1:0,x2:1,y2:1}]));
+    expect(serializer.deserialize('AAc00AA')).toStrictEqual(new CharacterState(-1,-1,3,[{x1:0,y1:0,x2:-1,y2:-1}]));
     expect(() => {
-        characterStateSerializer.deserialize(text)
-    }).toThrowError(/^Invalid character position xPos=undefined, yPos=1, direction=3$/);
-    text = 'aab3322';
+        serializer.deserialize('3ac00AA')
+    }).toThrowError(/^Unrecognized position character: '3'$/);
     expect(() => {
-        characterStateSerializer.deserialize(text)
-    }).toThrowError(/^Invalid path coordinates x1=undefined y1=undefined x2=undefined y2=undefined$/);
+        serializer.deserialize('aab3322')
+    }).toThrowError(/^Unrecognized position character: '3'$/);
 });
 
-test('getDirectionCoord', () => {
+test('encodeDirection', () => {
     expect.assertions(8);
-    const characterStateSerializer = new CharacterStateSerializer();
-    expect(characterStateSerializer.getDirectionCoord(0)).toBe('0');
-    expect(characterStateSerializer.getDirectionCoord(1)).toBe('a');
-    expect(characterStateSerializer.getDirectionCoord(2)).toBe('b');
-    expect(characterStateSerializer.getDirectionCoord(3)).toBe('c');
-    expect(characterStateSerializer.getDirectionCoord(4)).toBe('d');
-    expect(characterStateSerializer.getDirectionCoord(5)).toBe('e');
-    expect(characterStateSerializer.getDirectionCoord(6)).toBe('f');
-    expect(characterStateSerializer.getDirectionCoord(7)).toBe('g');
+    const serializer = new CharacterStateSerializer();
+    expect(serializer.encodeDirection(0)).toBe('0');
+    expect(serializer.encodeDirection(1)).toBe('a');
+    expect(serializer.encodeDirection(2)).toBe('b');
+    expect(serializer.encodeDirection(3)).toBe('c');
+    expect(serializer.encodeDirection(4)).toBe('d');
+    expect(serializer.encodeDirection(5)).toBe('e');
+    expect(serializer.encodeDirection(6)).toBe('f');
+    expect(serializer.encodeDirection(7)).toBe('g');
 });
 
-test('getDirectionFromCoord', () => {
+test('decodeDirection', () => {
     expect.assertions(9);
-    const characterStateSerializer = new CharacterStateSerializer();
-    expect(characterStateSerializer.getDirectionFromCoord('0')).toBe(0);
-    expect(characterStateSerializer.getDirectionFromCoord('a')).toBe(1);
-    expect(characterStateSerializer.getDirectionFromCoord('b')).toBe(2);
-    expect(characterStateSerializer.getDirectionFromCoord('c')).toBe(3);
-    expect(characterStateSerializer.getDirectionFromCoord('d')).toBe(4);
-    expect(characterStateSerializer.getDirectionFromCoord('e')).toBe(5);
-    expect(characterStateSerializer.getDirectionFromCoord('f')).toBe(6);
-    expect(characterStateSerializer.getDirectionFromCoord('g')).toBe(7);
+    const serializer = new CharacterStateSerializer();
+    expect(serializer.decodeDirection('0')).toBe(0);
+    expect(serializer.decodeDirection('a')).toBe(1);
+    expect(serializer.decodeDirection('b')).toBe(2);
+    expect(serializer.decodeDirection('c')).toBe(3);
+    expect(serializer.decodeDirection('d')).toBe(4);
+    expect(serializer.decodeDirection('e')).toBe(5);
+    expect(serializer.decodeDirection('f')).toBe(6);
+    expect(serializer.decodeDirection('g')).toBe(7);
     expect(() => {
-        characterStateSerializer.getDirectionFromCoord('3')
-    }).toThrowError(/^Unrecognized direction coordinate 3$/);
+        serializer.decodeDirection('3')
+    }).toThrowError(/^Unrecognized direction character 3$/);
 });
 
-test('getAlphabetCoord', () => {
+test('encodePosition', () => {
+    expect.assertions(8);
+    const serializer = new CharacterStateSerializer();
+    expect(serializer.encodePosition(0)).toBe('0');
+    expect(serializer.encodePosition(-1)).toBe('A');
+    expect(serializer.encodePosition(1)).toBe('a');
+    expect(serializer.encodePosition(-26)).toBe('Z');
+    expect(serializer.encodePosition(26)).toBe('z');
+    expect(serializer.encodePosition(-130)).toBe('Z');
+    expect(serializer.encodePosition(34)).toBe('z');
+    expect(() => {
+        serializer.encodePosition(NaN)
+    }).toThrowError(/^Position out of the range: NaN$/);
+});
+
+test('decodePosition', () => {
     expect.assertions(7);
-    const characterStateSerializer = new CharacterStateSerializer();
-    expect(characterStateSerializer.getAlphabetCoord(0)).toBe('0');
-    expect(characterStateSerializer.getAlphabetCoord(-1)).toBe('A');
-    expect(characterStateSerializer.getAlphabetCoord(1)).toBe('a');
-    expect(characterStateSerializer.getAlphabetCoord(-26)).toBe('Z');
-    expect(characterStateSerializer.getAlphabetCoord(26)).toBe('z');
-    expect(characterStateSerializer.getAlphabetCoord(-130)).toBe('Z');
-    expect(characterStateSerializer.getAlphabetCoord(34)).toBe('z');
-})
+    const serializer = new CharacterStateSerializer();
+    expect(serializer.decodePosition('0')).toBe(0);
+    expect(serializer.decodePosition('A')).toBe(-1);
+    expect(serializer.decodePosition('a')).toBe(1);
+    expect(serializer.decodePosition('Z')).toBe(-26);
+    expect(serializer.decodePosition('z')).toBe(26);
+    expect(() => {
+        serializer.decodePosition('')
+    }).toThrowError(/^Unrecognized position character: ''$/);
+    expect(() => {
+        serializer.decodePosition('!')
+    }).toThrowError(/^Unrecognized position character: '!'$/);
+});
