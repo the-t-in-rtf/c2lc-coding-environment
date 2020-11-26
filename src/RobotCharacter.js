5be231356a775601f2a84a26a0876b61272b0903
Daniel Cho <chosww@gmail.com>
2020-11-26 15:59:07 -0500

    C2LC-262: Update RobotCharacter to render character icon based on the theme

--- src/RobotCharacter.js
+++ src/RobotCharacter.js
@@ -1,26 +1,55 @@
 // @flow
 
 import React from 'react';
+import { ReactComponent as RobotIcon } from './svg/Robot.svg';
+import { ReactComponent as SpaceShipIcon } from './svg/SpaceShip.svg';
 import { ReactComponent as RabbitIcon } from './svg/Rabbit.svg';
 import './RobotCharacter.scss';
 
 type RobotCharacterProps = {
+    theme: string,
     transform: string,
     width: number
 };
 
 export default class RobotCharacter extends React.Component<RobotCharacterProps, {}> {
-    render() {
-        return (
-            <g
-                className='RobotCharacter'
-                transform={this.props.transform}>
+    getThemedCharacter = () => {
+        if (this.props.theme === 'space') {
+            return (
+                <SpaceShipIcon
+                    className='RobotCharacter__icon'
+                    x={-this.props.width/2}
+                    y={-this.props.width/2}
+                    width={this.props.width}
+                    height={this.props.width} />
+            )
+        } else if (this.props.theme === 'forest') {
+            return (
                 <RabbitIcon
                     className='RobotCharacter__icon'
                     x={-this.props.width/2}
                     y={-this.props.width/2}
                     width={this.props.width}
                     height={this.props.width} />
+            )
+        } else {
+            return (
+                <RobotIcon
+                    className='RobotCharacter__icon'
+                    x={-this.props.width/2}
+                    y={-this.props.width/2}
+                    width={this.props.width}
+                    height={this.props.width} />
+            )
+        }
+    }
+
+    render() {
+        return (
+            <g
+                className='RobotCharacter'
+                transform={this.props.transform}>
+                {this.getThemedCharacter()}
             </g>
         );
     }
---
+++ src/RobotCharacter.test.js
@@ -0,0 +1,51 @@
+// @flow
+
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount } from 'enzyme';
+import RobotCharacter from './RobotCharacter';
+
+configure({ adapter: new Adapter() });
+
+const defaultRobotCharacterProps = {
+    theme: 'default',
+    transform: '',
+    width: 3
+};
+
+function createMountRobotChracter(props) {
+    const wrapper = mount(
+        React.createElement(
+            RobotCharacter,
+            (Object.assign(
+                {},
+                defaultRobotCharacterProps,
+                props
+            ):RobotCharacter)
+        )
+    );
+
+    return wrapper;
+}
+
+function findRobotCharacter(wrapper) {
+    return wrapper.find('.RobotCharacter__icon');
+}
+
+describe('Right character should render based on theme props', () => {
+    test('default', () => {
+        expect.assertions(1);
+        const robotCharacterWrapper = createMountRobotChracter();
+        expect(findRobotCharacter(robotCharacterWrapper).get(0).type.render().props.children).toBe('Robot.svg');
+    });
+    test('forest', () => {
+        expect.assertions(1);
+        const robotCharacterWrapper = createMountRobotChracter({theme: 'forest'});
+        expect(findRobotCharacter(robotCharacterWrapper).get(0).type.render().props.children).toBe('Rabbit.svg');
+    });
+    test('space', () => {
+        expect.assertions(1);
+        const robotCharacterWrapper = createMountRobotChracter({theme: 'space'});
+        expect(findRobotCharacter(robotCharacterWrapper).get(0).type.render().props.children).toBe('SpaceShip.svg');
+    });
+})
