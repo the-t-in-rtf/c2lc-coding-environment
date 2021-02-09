dfc2c748ec081542de186a44b936c5078a259016
Daniel Cho <chosww@gmail.com>
2021-02-09 17:12:55 -0500

    C2LC-286: Update StopButton implementation to take className property

--- src/StopButton.js
+++ src/StopButton.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import AriaDisablingButton from './AriaDisablingButton';
+import classNames from 'classnames';
 import { injectIntl } from 'react-intl';
 import type { IntlShape } from 'react-intl';
 import { ReactComponent as StopIcon } from './svg/Stop.svg';
@@ -9,16 +10,21 @@ import './StopButton.scss';
 
 type StopButtonProps = {
     intl: IntlShape,
+    className: string,
     disabled: boolean,
     onClick: () => void
 };
 
 class StopButton extends React.Component<StopButtonProps, {}> {
     render() {
+        const classes = classNames(
+            this.props.className,
+            'StopButton'
+        );
         return (
             <AriaDisablingButton
                 aria-label={`${this.props.intl.formatMessage({id:'StopButton'})}`}
-                className='StopButton'
+                className={classes}
                 disabledClassName='StopButton--disabled'
                 disabled={this.props.disabled}
                 onClick={this.props.onClick}
